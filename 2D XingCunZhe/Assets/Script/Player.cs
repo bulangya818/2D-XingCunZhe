9ca8f2003bd8d67c6d935af75b85654edc5f3884
bulangya818 <139843735+bulangya818@users.noreply.github.com>
2025-12-13 21:54:03 +0800

    角色创建

---
+++ 2D XingCunZhe/Assets/Script/Player.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+
+    private Rigidbody2D rigid;
+    private SpriteRenderer spriter;
+    private Animator anim;
+    
+    //Awake is called before the first frame update
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        
+    }
+
+    private void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+       
+        Vector2 nextVec =inputVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+
+    }
+    
+    private void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
+    }
+}
