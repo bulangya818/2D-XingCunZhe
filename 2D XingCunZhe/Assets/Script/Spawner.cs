cba41d395d67d98f47852c364e45f3bec8a61700
bulangya818 <139843735+bulangya818@users.noreply.github.com>
2025-12-16 23:03:58 +0800

    随机生成怪物

---
+++ 2D XingCunZhe/Assets/Script/Spawner.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public Transform[] spawnPoint;
+
+    private float timer;
+
+    private void Awake()
+    {
+        spawnPoint = GetComponentsInChildren<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer>0.2f)
+        {
+            timer = 0;
+            Spawn();
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            GameManager.instance.pool.Get(0);
+        }
+    }
+
+    void Spawn()
+    {
+        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0, 2));
+        enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
+    }
+}
